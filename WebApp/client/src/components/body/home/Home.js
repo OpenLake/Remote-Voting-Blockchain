8fa612ff760a16b2c8d0d0d39d1725f59dd3ef8a
Ananya <65886348+Ananyaiitbhilai@users.noreply.github.com>
2021-06-09 16:06:12 +0530

    verification of email addresses

---
+++ WebApp/client/src/components/body/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                This site verifies the email of the user who registers for the very first time.
+            </p>
+            
+        </div>
+    )
+}
+
+export default Home
